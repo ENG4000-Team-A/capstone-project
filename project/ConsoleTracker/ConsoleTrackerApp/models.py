f0db517a41678c92424719a11eeb75008c9267cf
Eric Kwok <46385457+erick576@users.noreply.github.com>
2021-11-02 13:07:15 -0400

    Add timestamp model (#31)
    Co-authored-by: Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>

--- project/ConsoleTracker/ConsoleTrackerApp/models.py
+++ project/ConsoleTracker/ConsoleTrackerApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class TimeStamp(models.Model):
+    hours = models.PositiveIntegerField(default=0)
+    # Use Validators to restrict minutes and seconds to [1, 60]. ValidationError will be raised if not within the range
+    minutes = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(60)], default=0)
+    seconds = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(60)], default=0)
