021a245764ec118ab085bebd2c8ce8fc095258b1
Jessey H <jessey@barebrand.ca>
2021-11-12 13:21:27 -0500

    Initial Push
    added new view and countdown page template to the files

--- project/ConsoleTracker/ConsoleTrackerApp/views.py
+++ project/ConsoleTracker/ConsoleTrackerApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def timer(request):
+	username = 'User12345'
+	timeLeft = 3000
+	Machine_ID = 1
+	return render(request, "countdown.html", {"username": username, "Machine_ID": Machine_ID, "timeLeft": timeLeft})
