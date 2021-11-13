a3f0e971f9d7d9bed9f440280653673062118996
Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>
2021-11-12 20:59:25 -0500

    Add machine model

--- project/ConsoleTracker/ConsoleTrackerApp/models.py
+++ project/ConsoleTracker/ConsoleTrackerApp/models.py
@@ -7,3 +7,14 @@ class TimeStamp(models.Model):
     # Use Validators to restrict minutes and seconds to [1, 60]. ValidationError will be raised if not within the range
     minutes = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(60)], default=0)
     seconds = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(60)], default=0)
+
+
+class Machine(models.Model):
+    name = models.TextField(blank=True, null=False)
+    active = models.BooleanField(default=False)
+    # TODO (Future User Model will be mapped to a machine
+    # user = models.OneToOneField(
+    #     User,
+    #     on_delete=models.CASCADE,
+    #     foreign_key=True,
+    # )
