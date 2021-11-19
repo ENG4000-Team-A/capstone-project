a398ca342f6f9c84e53fa14c4e35eef40d804b1e
Jessey H <jessey@barebrand.ca>
2021-11-19 08:05:35 -0500

    Initialized Machine List Page
    Added machine list page to app

--- project/ConsoleTracker/ConsoleTrackerApp/views.py
+++ project/ConsoleTracker/ConsoleTrackerApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Machine
 
 # Create your views here.
 
@@ -8,3 +10,6 @@ def timer(request):
 	timeLeft = 3000
 	Machine_ID = 1
 	return render(request, "countdown.html", {"username": username, "Machine_ID": Machine_ID, "timeLeft": timeLeft})
+
+class machines(ListView):
+	model = Machine
