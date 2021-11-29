94c71db36fa52fa58f925b409d3ca4101d36bcd8
Eric Kwok <46385457+Kwok-Eric-216397150@users.noreply.github.com>
2021-11-29 13:35:37 -0500

    Add Tests for Models

--- project/ConsoleTracker/ConsoleTracker/urls.py
+++ project/ConsoleTracker/ConsoleTracker/urls.py
@@ -15,15 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ConsoleTrackerApp import views
+from ConsoleTrackerApp import views, tasks
 from django.conf import settings
 from django.conf.urls.static import static
-from ConsoleTrackerApp import tasks
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('timer/', views.timer, name = 'timer'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('timer/', views.timer, name='timer'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # runs the time checking background task at startup
 tasks.main()
