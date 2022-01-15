5b54cef7c6e232505e9e654b3c267cc0929e5e4d
digengill <gilldigen@gmail.com>
2022-01-14 22:58:38 -0500

    Init react code
    Setup react router & associated components

---
+++ project/frontend/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Login.css'
+function Login() {
+    return (
+        <div className='login__container'>
+            login
+        </div>
+    )
+}
+
+export default Login
---
+++ project/frontend/src/components/MachineInfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MachineInfo.css'
+function MachineInfo() {
+    return (
+        <div className='machineinfo__container'>
+            machine info
+        </div>
+    )
+}
+
+export default MachineInfo
