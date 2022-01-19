dd582d823f1a3b08dd5edb17aea7b76cab2e530e
elie0174 <36248350+elie0174@users.noreply.github.com>
2022-01-19 15:46:27 -0500

    machine info page
    added the machine info page react

--- project/frontend/src/components/MachineInfo.js
+++ project/frontend/src/components/MachineInfo.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MachineInfo.css'
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+
+
+function SubmitPlay() {
+    console.log(SubmitPlay);
+}
+
 function MachineInfo() {
+    const [userInfo, SetUserInfo] = useState([{ username: "Elie", time: 1.2, machineName: null, machinetype: "xbox" }]);
+
     return (
         <div className='machineinfo__container'>
-            machine info
+            <div>
+                {userInfo.map(user => (
+                    <div key={user.username}>
+                        <p>
+                            {"UserName: "}
+                            {user.username}
+                        </p>
+                        <p>
+                            {"Machine Name: "}
+                            {user.machineName}
+                        </p>
+                        <p >
+                            {"Machine Type: "}
+                            {user.machinetype}
+                        </p>
+                        <p>
+                            {"Time left On account (hours): "}
+                            {user.time}
+                       </p>
+                    </div>
+
+
+                ))}
+            </div>
+            <div>
+                <Button variant="contained" color="success" onClick={SubmitPlay}>
+                    Start Playing
+                </Button>
+            </div>
         </div>
+
     )
 }
 
