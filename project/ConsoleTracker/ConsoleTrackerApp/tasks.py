7bc479d05ebd2333ac7ad0da25e10f3fe56939e8
digengill <gilldigen@gmail.com>
2022-01-21 23:41:13 -0500

    Updates smart switches periodically to make sure their state matches the corresponding Machine's state in the db

--- project/ConsoleTracker/ConsoleTrackerApp/tasks.py
+++ project/ConsoleTracker/ConsoleTrackerApp/tasks.py
@@ -4,7 +4,11 @@ from threading import Thread
 from django.utils import timezone
 import asyncio
 import time
+from kasa import Discover
+import sys
 
+# Period in seconds in between syncing switches to machine state
+SYNC_PERIOD = 10
 
 # seperated out because nested function calls didnt work
 def turn_off_switch_help(ip):
@@ -25,6 +29,7 @@ def switch_on(ip):
 
 
 def query_time():
+    i = 0
     while True:
         # like an SQL SELECT where only looking for unexpired timers, from today,
         # with end_time before now
@@ -43,10 +48,37 @@ def query_time():
         # minimum 1 second between loops, but usually 1.01s on my machine.
         # may need to change value to guarantee 1s between loops
         time.sleep(1)
+        i += 1
+        if i >= SYNC_PERIOD:
+            i = 0
+            sync_switch_states()
 
+
+def sync_switch_states():
+    """
+    Syncs smart switches to match the Machine states.
+    """
+    print("Checking switch states")
+    devices = asyncio.run(Discover.discover()) # Scan for devices
+    machines = Machine.objects.all() # Get machines
+    for m in machines:
+        for addr, dev in devices.items(): # addr is ip address, dev is SmartDevice
+            #asyncio.run(dev.update())
+            if (m.ip == addr):
+                if (dev.is_on and m.active == False ):
+                    asyncio.run(dev.turn_off())
+                    print("Shutting down machine with ip: {ip}".format(ip=addr))
+                elif (not dev.is_on and m.active == True):
+                    asyncio.run(dev.turn_on())
+                    print("Powering on machine with ip: {ip}".format(ip=addr))
+                else:
+                    print("State okay")
+                    
 
 # runs query_time in a background thread
 def start_query_daemon():
+    if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'): # windows bug https://github.com/encode/httpx/issues/914
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     t = Thread(target=query_time)
     t.daemon = True
     t.start()
