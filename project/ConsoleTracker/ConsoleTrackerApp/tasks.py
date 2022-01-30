0f3865a71808ad0fd435ab0aa20d3c4802a99c7b
Jessey H <jessey.harry.23@gmail.com>
2022-01-29 21:06:58 -0500

    Update external db time
    Updates external db time by set interval at every interval (currently set to 15 seconds)
    
    Update project/ConsoleTracker/ConsoleTrackerApp/tasks.py
    
    Refactor query_time
    
    Refactor query_time to separate the different functions in the thread. Noticed that the sync_switch_states takes a little bit of time and causes the thread to hang. We might have to switch this over to another thread.
    
    Also have to figure out a way to update the time if the machine is closed via the update_expired_machines function
    
    @Thanh2311 @digengill lmk what you think.
    
    PR: #123
    Co-Authored-By: Thanh2311 <59661596+Thanh2311@users.noreply.github.com>

--- project/ConsoleTracker/ConsoleTrackerApp/tasks.py
+++ project/ConsoleTracker/ConsoleTrackerApp/tasks.py
@@ -1,9 +1,13 @@
+from tracemalloc import start
 from .models import Machine, User_uses_machine
 from .scripts.SwitchScript import ConsolSwitch
+from .InternalSocketConnect import InternalSocket
 from threading import Thread
 from django.utils import timezone
 import asyncio
+import datetime
 import time
+import pytz
 from kasa import Discover
 import sys
 
@@ -27,31 +31,80 @@ def switch_on(ip):
 	t = Thread(target=turn_on_switch_help, args=(ip,))
 	t.start()
 
+# runs the update time function 
+def update_time(socket, username, time):
+    return socket.update_time(username, time)
+
+#thread to update the time balance of the user
+def update_time_thread(socket):
+    #represents the seconds on the clock we will update time
+    check_seconds = [00, 15, 30, 45]
+    interval = 15
+
+    #The current system time and the seconds of the current clock
+    curr_time = timezone.now()
+    clock_second = int(curr_time.strftime('%S'))
+
+    print(f"{curr_time.strftime('%S')} {clock_second in check_seconds}", end="\r")
+    
+    #if the current clock second is equal to one of the interval times update users' time
+    if clock_second in check_seconds :
+        #queries for all active machines
+        active_machines = User_uses_machine.objects.filter(
+            expired = False
+            )
+        #print(active_machines)
+        #loop through each active machine
+        for machine in active_machines:
+            #determine the seconds between the start of the machine and the current time to figure out if the machine
+            # started less than the interval
+            start_dif = int((curr_time - machine.start_time).total_seconds())
+            #print(f"{curr_time} {machine.start_time}")
+            #print(f"{start_dif}")
+            
+            if (start_dif < interval) :
+                #If the machine started less than interval update time by difference
+                update_time(socket, machine.user.username, start_dif)
+                print(f"updating {machine.user.username} time by {start_dif} seconds")
+            else :
+                #else update by interval
+                update_time(socket, machine.user.username, interval)
+                print(f"updating {machine.user.username} time by {interval} seconds")
+
+def update_expired_machines() :
+    # like an SQL SELECT where only looking for unexpired timers, from today,
+    # with end_time before now
+    query_set = User_uses_machine.objects.filter(
+        end_time__date=timezone.now().date(),
+        end_time__time__lte=timezone.now().time(),
+        expired=False)
+    for query in query_set:
+        query.machine.active = False
+        query.machine.save()
+        query.expired = True
+        query.save()
+        switch_off(query.machine.ip)
+        print(query.user.username + ' on ' + query.machine.name + ' ended at '
+                + query.end_time.strftime("%m/%d/%Y, %H:%M:%S") + ', set to inactive')
+
 
 def query_time():
     i = 0
+    #create the socket that connects to ExternalSocket
+    socket = InternalSocket()
+
     while True:
+        update_time_thread(socket)
         # like an SQL SELECT where only looking for unexpired timers, from today,
         # with end_time before now
-        query_set = User_uses_machine.objects.filter(
-            end_time__date=timezone.now().date(),
-            end_time__time__lte=timezone.now().time(),
-            expired=False)
-        for query in query_set:
-            query.machine.active = False
-            query.machine.save()
-            query.expired = True
-            query.save()
-            switch_off(query.machine.ip)
-            print(query.user.username + ' on ' + query.machine.name + ' ended at '
-                  + query.end_time.strftime("%m/%d/%Y, %H:%M:%S") + ', set to inactive')
-        # minimum 1 second between loops, but usually 1.01s on my machine.
-        # may need to change value to guarantee 1s between loops
-        time.sleep(1)
-        i += 1
+        update_expired_machines()
         if i >= SYNC_PERIOD:
             i = 0
             sync_switch_states()
+        i += 1
+        # minimum 1 second between loops, but usually 1.01s on my machine.
+        # may need to change value to guarantee 1s between loops
+        time.sleep(1)
 
 
 def sync_switch_states():
@@ -76,7 +129,7 @@ def sync_switch_states():
 
 
 def stop_timer(active_timer :User_uses_machine):
-	"""
+    """
 	Sets endtime of an active timer to now.
 	Sets users time to remaining timer value.
 	"""
