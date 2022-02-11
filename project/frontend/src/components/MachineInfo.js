5a890e3b12908134957b5aeee743e00b2518fe61
elie0174 <36248350+elie0174@users.noreply.github.com>
2022-02-10 19:21:41 -0500

    updated

--- project/frontend/src/components/MachineInfo.js
+++ project/frontend/src/components/MachineInfo.js
@@ -9,33 +9,65 @@ function SubmitPlay() {
 }
 
 function MachineInfo() {
-    const [userInfo, SetUserInfo] = useState([{ username: "Elie", time: 1.2, machineName: null, machinetype: "xbox" }]);
+    const { id } = useParams()
+    const [machine, setMachine] = useState([]);
+    const [userInfo, SetUserInfo] = useState();
+    
+useEffect(()=>{
+    axios.get("http://localhost:8000/getMachines/"+id
+    ).then(response => {
+        setMachine(response.data.data)
+        console.log(response.data)
 
+    }).catch(error => {
+        console.log(error)
+    })
+
+},[]);
+useEffect(() => {
+    const username = localStorage.getItem("user");
+    setName(username);
+    console.log(username);
+    axios.get(`http://localhost:8000/users/?uname=${username}`)
+        .then(response => {
+            console.log(response.data.data);
+            SetUserInfo(response.data.data)
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+}, []);
     return (
         <div className='machineinfo__container'>
             <div>
                 {userInfo.map(user => (
-                    <div key={user.username}>
+                    <div key={user.first_name}>
                         <p>
                             {"UserName: "}
-                            {user.username}
-                        </p>
-                        <p>
-                            {"Machine Name: "}
-                            {user.machineName}
-                        </p>
-                        <p >
-                            {"Machine Type: "}
-                            {user.machinetype}
+                            {user.first_name}
                         </p>
                         <p>
                             {"Time left On account (hours): "}
                             {user.time}
                        </p>
                     </div>
-
-
                 ))}
+                
+                {machine.map(machine => (
+                    <div key={machine.id}>
+                       <p>
+                            {"Machine Name: "}
+                            {machine.name}
+                        </p>
+                        <p >
+                            {"Machine Type: "}
+                            {machine.machine_type}
+                        </p>
+                      
+                       </div>
+                ))}
+                 
             </div>
             <div>
                 <Button variant="contained" color="success" onClick={SubmitPlay}>
